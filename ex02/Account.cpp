79fc411faac8c36b855f2da001708da1d6db1693
Yassine fahfouhi <yassinefahfouhi@Yassines-MacBook-Pro.local>
2025-10-27 22:50:23 +0100

    to be continued

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yafahfou <yafahfou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yassinefahfouhi <yassinefahfouhi@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 19:13:48 by yafahfou          #+#    #+#             */
-/*   Updated: 2025/10/27 20:27:03 by yafahfou         ###   ########.fr       */
+/*   Updated: 2025/10/27 22:50:06 by yassinefahf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,31 +48,38 @@ int Account::getNbWithdrawals()
 
 void Account::displayAccountsInfos()
 {
-    std::cout<<"accounts:"<<_nbAccounts<<";";
-    std::cout<<"total:"<<_totalAmount<<";";
-    std::cout<<"deposits:"<<_totalNbDeposits<<";";
-    std::cout<<"withdrawals:"<<_totalNbWithdrawals<<std::endl;
+    std::cout << "accounts:" << _nbAccounts << ";";
+    std::cout << "total:" << _totalAmount << ";";
+    std::cout << "deposits:" << _totalNbDeposits << ";";
+    std::cout << "withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
-void    Account::makeDeposit(int deposit)
+void Account::makeDeposit(int deposit)
 {
     _nbDeposits = 1;
     _totalAmount += deposit;
     _totalNbDeposits++;
 }
 
-void    Account::displayStatus() const {
-    
-    std::cout<<"index:"<<_accountIndex<<";";
-    if (_nbDeposits > 1)
-        std::cout<<"p_amount:"<<_amount<<";";
-    else
-        std::cout<<"amount"<<_amount;
-    std::cout<<"deposit:"<<_nbDeposits<<";";
-    
-}
-bool    Account::makeWithdrawal(int withdrawal)
+void Account::displayStatus() const
 {
-    
+
+    std::cout << "index:" << _accountIndex << ";";
+    if (_nbDeposits > 1)
+        std::cout << "p_amount:" << _amount << ";";
+    else
+        std::cout << "amount" << _amount;
+    std::cout << "deposit:" << _nbDeposits << ";";
+}
+bool Account::makeWithdrawal(int withdrawal)
+{
 }
 
+void Account::_displayTimestamp()
+{
+    char timeStamp[20];
+    std::time_t present;
+
+    std::time(&present);
+    std::strftime(timeStamp, sizeof(timeStamp), "%Y%m%d_%H%M%S", std::localtime(&present));
+    std::cout << "[" << timeStamp << "]";
